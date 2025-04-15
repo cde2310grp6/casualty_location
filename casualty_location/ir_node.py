416e61ec37cf22b9b1813ff9bd5422bad476de40
ffluryy <kaijie.chong@gmail.com>
2025-04-15 21:29:29 +0800

    changed publishing for ir

--- casualty_location/ir_node.py
+++ casualty_location/ir_node.py
@@ -2,10 +2,10 @@ import time
 import sys
 import casualty_location.amg8833_i2c_driver as amg8833_i2c
 import numpy as np
-import cv2
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from custom_msg_srv.msg import IrRaw
 
 # Debugging values removed
 
@@ -13,7 +13,7 @@ from std_msgs.msg import String
 class IRPubNode(Node):
     def __init__(self):
         super().__init__('ir_pub_node')
-        self.publisher = self.create_publisher(String, 'ir_data', 10)
+        self.publisher = self.create_publisher(String, 'ir_raw_data', 10)
 
         self.sensor = self.initialize_sensor()
         if self.sensor is None:
@@ -46,18 +46,17 @@ class IRPubNode(Node):
         pix_res = (8, 8)
 
         status, pixels = self.sensor.read_temp(pix_to_read)
-
         if not status:
             z = np.reshape(pixels, pix_res)
             for i in range(4):
                 z = np.delete(z, 0, axis=0)
 
             # Get the highest value from each column
-            max_in_columns = np.max(z, axis=0)  # Max value per column
+            #max_in_columns = np.max(z, axis=0)  # Max value per column
 
             # Publish the result as a 1D array
-            msg = String()
-            msg.data = str(max_in_columns.tolist())  # Convert to list and publish
+            msg = IrRaw
+            msg.ir_data = pixels  # Convert to list and publish
             self.publisher.publish(msg)
             self.get_logger().info(f"Publishing IR data: {msg.data}")
 
